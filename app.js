aba8c7f2e12f9ce6a26e9fc9a209af7a627cd992
Lucas de Lima <lucasdelima.neves@gmail.com>
2020-11-17 13:08:32 -0300

    revisando códigos

--- app.js
+++ app.js
@@ -51,21 +51,9 @@ app.post('/home', async (req, res) => {
 
     const dados = {
         "topTitulo": "Temos a solução que a sua empresa precisa!",
-        "topSubtitulo": "This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+        "topSubtitulo": "Faça um orçamento.",
         "topTextoBtn": "ENTRE EM CONTATO",
         "topLinkBtn": "http://localhost:3000/",
-
-        "serTitulo": "Serviços",
-        "serSubtitulo": "Featured content or information",
-        "serUmIcone": "code",
-        "serUmTitulo": "Serviço 1",
-        "serUmDesc": "Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
-        "serDoisIcone": "laptop-code",
-        "serDoisTitulo": "Serviço 2",
-        "serDoisDesc": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
-        "serTresIcone": "mobile-alt",
-        "serTresTitulo": "Serviço 3",
-        "serTresDesc": "Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
     }
 
     const homeExiste = await Home.findOne({});
